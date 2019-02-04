e56eeba7b0ed45891decccb437aad302d3229764
Ron Rittinger <ron.rittinger@kroger.com>
2019-02-04 09:53:04 -0500

    Adding json-pubsub example

---
+++ json-pubsub/Publish.js
@@ -0,0 +1,30 @@
+'use strict';
+ 
+const {PubSub} = require('@google-cloud/pubsub');
+ 
+const publisher = new PubSub()
+                .topic("testTopic")
+                .publisher();
+ 
+exports.publish = (req, res) => {
+ 
+  // Convert request message to JSON for pubsub
+  const jsonMessage = {
+    data: {
+      message: req.query.message
+    },
+  };
+  const dataBuffer = Buffer.from(
+    JSON.stringify(jsonMessage)
+  );
+ 
+  // Publishe the message
+  return publisher
+   .publish(dataBuffer)
+    .then(() => res.status(200).send('Message published.'))
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+      return Promise.reject(err);
+    });
+};
